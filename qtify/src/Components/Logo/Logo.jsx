8e1767223bc91dd647384ce908b9d9739aec3968
saran303 <saran13051999@gmail.com>
2024-01-31 19:45:01 +0530

    Navbar completed

---
+++ qtify/src/Components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LogoImg from '../../assets/logoImg.png'
+
+const Logo = () => {
+  return (
+    <img src={LogoImg} alt="LogoImage" width={67} height={34}/>
+  )
+}
+
+export default Logo
