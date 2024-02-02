bf9cf377dc40af76bd83dbfd79bfc65293898573
saran303 <saran13051999@gmail.com>
2024-02-02 23:35:14 +0530

    Navbar and hero

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
+import Hero from './Components/Hero/Hero';
 import Nav from './Components/Navbar/Nav';
+import { fetchTopAlbums } from './api/api';
+// import { Card } from './Components/Card';
+// import Section from './Components/Section/Section';
 
 function App() {
+const [topAlbumsData, setTopAlbumsData] = useState([]);
+
+const generateTopAlbums = async () => {
+  try{
+    const data = await fetchTopAlbums();
+    setTopAlbumsData(data);
+  }catch(err){
+    console.error(err);
+  }
+};
+
+useEffect(() => {
+    generateTopAlbums();
+},[])
   return (
     <div>
       <Nav />
+      <Hero />
+      {/* {
+        topAlbumsData.map((topAlbum) => 
+        <Card data={topAlbum} type='album' key={topAlbum.id}/>
+        )
+      } */}
+      {/* <Section data={topAlbumsData} title="Top Albums" type="album"/> */}
     </div>
   );
 }
--- qtify/src/Components/Logo/Logo.jsx
+++ qtify/src/Components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import LogoImg from '../../assets/logoImg.png'
+import styles from './Logo.module.css'
 
 const Logo = () => {
   return (
-    <img src={LogoImg} alt="LogoImage" width={67} height={34}/>
+    <img src={LogoImg} alt="LogoImage" width={67} height={34} className={styles.logo} />
   )
 }
 
